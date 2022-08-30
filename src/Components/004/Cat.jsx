7fb524fbcc580bb2c11257070ae6d40b6dafed58
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-08-31 01:45:06 +0300

    Cats beeing in the circle,  who weight > 4

--- src/Components/004/Cat.jsx
+++ src/Components/004/Cat.jsx
@@ -1,6 +1,11 @@
 function Cat({ cat, i }) {
   return (
-    <div style={{ backgroundColor: i % 2 ? "red" : "dodgerBlue" }}>
+    <div
+      style={{
+        backgroundColor: i % 2 ? "red" : "dodgerBlue",
+        borderRadius: cat.weight > 4 ? "50px" : 0,
+      }}
+    >
       {cat.name} {cat.weight}
     </div>
   );
