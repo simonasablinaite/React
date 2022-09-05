0261cd7b562471a1232349737d013ebaacdb5c04
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-09-05 13:32:57 +0300

    correct state update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.scss";
 function App() {
   const [color, setColor] = useState("blue");
   const [fontSize, setFontSize] = useState(20);
+  const [count, setCount] = useState(1);
 
   const doJob = () => {
     console.log("Hello");
@@ -12,15 +13,24 @@ function App() {
   const clicked = () => {
     setColor("yellowgreen");
   };
+
   const changeSize = () => {
     setFontSize(60);
   };
+
+  const doCount = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{ color: color, fontSize: fontSize }}>STATE</h1>
+        <h1 style={{ color: color, fontSize: fontSize }}>STATE {count}</h1>
         <button onClick={clicked}>Click!</button>
         <button onClick={changeSize}>Change size</button>
+        <button onClick={doCount}>+1</button>
       </header>
     </div>
   );
