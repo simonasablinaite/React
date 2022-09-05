c382e2a14eb68174f93567366d80f5fe34c0d83a
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-09-05 11:30:33 +0300

    State change font size

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 
 function App() {
   const [color, setColor] = useState("blue");
+  const [fontSize, setFontSize] = useState(20);
 
   const doJob = () => {
     console.log("Hello");
@@ -11,11 +12,15 @@ function App() {
   const clicked = () => {
     setColor("yellowgreen");
   };
+  const changeSize = () => {
+    setFontSize(60);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{ color: color }}>STATE</h1>
+        <h1 style={{ color: color, fontSize: fontSize }}>STATE</h1>
         <button onClick={clicked}>Click!</button>
+        <button onClick={changeSize}>Change size</button>
       </header>
     </div>
   );
